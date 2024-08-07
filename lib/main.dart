19c4b38b82f902ded23f3e6a707c7f73133ffb7a
Wamitinewton <wamitinewton@gmail.com>
2024-08-07 18:44:40 +0300

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,104 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'main.freezed.dart';
+part 'main.g.dart';
+
+@freezed
+class MyClass with _$MyClass {
+  factory MyClass({String? a, int? b}) = _MyClass;
+}
+
+@freezed
+class Union with _$Union {
+  const factory Union(int value) = Data;
+  const factory Union.loading() = Loading;
+  const factory Union.error([String? message]) = ErrorDetails;
+  const factory Union.complex(int a, String b) = Complex;
+
+  factory Union.fromJson(Map<String, Object?> json) => _$UnionFromJson(json);
+}
+
+@freezed
+class SharedProperty with _$SharedProperty {
+  factory SharedProperty.person({
+    String? name,
+    int? age,
+  }) = SharedProperty0;
+
+  factory SharedProperty.city({
+    String? name,
+    int? population,
+  }) = SharedProperty1;
+}
 
 void main() {
-  runApp(const MyApp());
-}
+  final myClassExample = MyClass(a: '42', b: 42);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  print(myClassExample.copyWith(a: null));
+  print(myClassExample.copyWith());
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  print(MyClass(a: '42', b: 42) == MyClass(a: '42', b: 42));
+  print(MyClass(a: '42', b: 42) == MyClass());
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  // destructuring pattern matching
+  const unionExample = Union(42);
+  print(
+    unionExample.when(
+      (value) => '$value',
+      loading: () => 'loading',
+      error: (message) => 'Error: $message',
+      complex: (a, b) => 'complex $a $b',
+    ),
+  );
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  print(
+    // maybeWhen allows some callbacks to be missing, but requires an `orElse` callback
+    unionExample.maybeWhen(
+      null,
+      loading: () => 'loading',
+      // voluntarily didn't pass error/complex callbacks
+      orElse: () => 42,
+    ),
+  ); //
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // works the same as `when`, but the callback is slightly different
+  print(
+    // `map` requires all callbacks to be not null
+    unionExample.map(
+      (Data value) => '$value',
+      loading: (Loading value) => 'loading',
+      error: (ErrorDetails error) => 'Error: ${error.message}',
+      complex: (Complex value) => 'complex ${value.a} ${value.b}',
+    ),
+  ); // 42
 
-  final String title;
+  print(
+    // maybeMap allows some callbacks to be missing, but requires an `orElse` callback
+    unionExample.maybeMap(
+      null,
+      error: (ErrorDetails value) => value.message,
+      // voluntarily didn't pass error/complex callbacks
+      orElse: () => 'fallthrough',
+    ),
+  ); // fallthrough
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+   // nice toString
+  print(const Union(42)); // Union(value: 42)
+  print(const Union.loading()); // Union.loading()
+  print(const Union.error(
+      'Failed to fetch')); // Union.error(message: Failed to fetch)
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // ------------------
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // shared properties between union possibilities
+  var example = SharedProperty.person(name: 'Remi', age: 24);
+  // OK, `name` is shared between both .person and .city constructor
+  print(example.name); // Remi
+  example = SharedProperty.city(name: 'London', population: 8900000);
+  print(example.name); // London
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  // COMPILE ERROR
+  // print(example.age);
+  // print(example.population);
 }
